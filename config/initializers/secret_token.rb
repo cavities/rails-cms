64ba8757e7e2cd21ff3e62535d14c85954d81337
cavities <cavities@sina.com>
2014-10-17 17:53:27 +0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCms::Application.config.secret_token = 'c79a407b0b3ee7a24ecf09c60676c7f9250888a8d200c17fab6ad8c75aef65f52c53d8da301f3408f27edc44d7500c2bd300fa3a8d14f66edd76fee6ca48a78c'
